c45c820682ffa109d75f5c1a4566df1aea0abe3d
Ruchir Shukla <ruchir@bizzappdev.com>
2017-10-03 19:03:55 +0530

    change import style

--- apps_download/__manifest__.py
+++ apps_download/__manifest__.py
@@ -22,6 +22,6 @@
     'installable': True,
     'active': False,
     'auto_install': False,
-    'application':False
+    'application': False
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
